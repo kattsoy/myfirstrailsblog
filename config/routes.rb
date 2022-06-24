f9258c80735d03a2c600ba3c92f1ed387dbf6504
«kattsoy» <kathkondratenko@gmail.com>
2022-06-24 15:07:59 +0200

    Model Posts added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :authors
   get 'home/index'
   root to: "home#index"
+
+  scope module: 'authors' do
+    resources :posts
+  end
+  
   resources :authors
 end
